e2a97a7cab99647ebf69576c259eace7c626de72
m-istighfar <istighfar.amal@gmail.com>
2024-05-07 06:35:45 +0700

    feat(profile-picture): enable profile picture uploads for lawyers

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,20 +4,22 @@ const { errorResponse } = require("../utils/response");
 
 const authenticationMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return errorResponse(res, "Unauthorized - No token provided", 401);
   }
 
   const token = authHeader.split(" ")[1];
-
   try {
     const decodedToken = jwt.verify(token, JWT_SIGN);
-    req.user = decodedToken;
+    if (decodedToken.role === "LAWYER") {
+      req.user = { lawyerId: decodedToken.lawyerId, role: decodedToken.role };
+    } else {
+      req.user = { userId: decodedToken.userId, role: decodedToken.role };
+    }
     next();
   } catch (error) {
     console.error(error);
-    errorResponse(res, "Unauthorized - Invalid token", 401);
+    return errorResponse(res, "Unauthorized - Invalid token", 401);
   }
 };
 
--- routes/uploadRoutes-gcp.js
+++ routes/uploadRoutes-gcp.js
@@ -13,18 +13,18 @@ router.post(
   authMiddleware,
 
   async (req, res) => {
-    console.log("Received request to upload profile picture");
-
     if (!req.file) {
-      console.log("No file uploaded");
       return res.status(400).send("No file uploaded.");
     }
 
-    const blob = bucket.file(
-      `profile-pictures/${req.user.userId}-${Date.now()}`
-    );
-    console.log("Blob created:", blob.name);
+    console.log(req.user.role);
 
+    const identifier = req.user.role === "LAWYER" ? "lawyerId" : "userId";
+    const idValue = req.user[identifier];
+
+    console.log(idValue);
+
+    const blob = bucket.file(`profile-pictures/${idValue}-${Date.now()}`);
     const blobStream = blob.createWriteStream({
       metadata: {
         contentType: req.file.mimetype,
@@ -38,21 +38,18 @@ router.post(
 
     blobStream.on("finish", async () => {
       const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-      console.log("File uploaded successfully. Public URL:", publicUrl);
 
       try {
-        const updatedUser = await prisma.user.update({
-          where: { id: req.user.userId },
+        const modelToUpdate =
+          req.user.role === "LAWYER" ? prisma.lawyer : prisma.user;
+        const updatedProfile = await modelToUpdate.update({
+          where: { id: idValue },
           data: { profilePictureUrl: publicUrl },
         });
-        console.log(
-          "Profile picture URL updated in the database:",
-          updatedUser
-        );
 
         res.status(200).send({ imageUrl: publicUrl });
       } catch (error) {
-        console.error("Failed to update user profile picture URL:", error);
+        console.error("Failed to update profile picture URL:", error);
         res
           .status(500)
           .send("Failed to update profile picture URL in the database.");
